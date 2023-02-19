f08c5239e5dfd95f2668913730e4e8a50f8d79c6
DanilaPodgornov <116427593+DanilaPodgornov@users.noreply.github.com>
2023-02-19 14:39:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,33 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    
+    int smallNumber = 0;
+    int largeNumber = 0;
+    int result = 0;
+    int modulo = 1;
+    int finalResult = 0;
+
+    if (b >= a)
+    {
+        largeNumber = b;
+        smallNumber = a;
+    }
+    else
+    {
+        largeNumber = a;
+        smallNumber = b;
+    }
+
+    while (modulo != 0)
+    {
+        finalResult = smallNumber;
+        result = largeNumber / smallNumber;
+        modulo = largeNumber % smallNumber;
+        largeNumber = smallNumber;
+        smallNumber = modulo;
+    }
+
+    return finalResult;
+    
 }
